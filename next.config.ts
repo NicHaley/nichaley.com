b9021eb9a14d93c5ae03600fa612042720229dd8
Nicholas Haley <hello@nichaley.com>
2025-08-25 20:47:50 -0400

    wip - installing mdx

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
+import nextMdx from "@next/mdx";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withMdx = nextMdx({
+  // By default only the `.mdx` extension is supported.
+  extension: /\.mdx?$/,
+  options: {
+    /* otherOptions… */
+  },
+});
+
+const nextConfig: NextConfig = withMdx({
+  // Support MDX files as pages:
+  pageExtensions: ["md", "mdx", "tsx", "ts", "jsx", "js"],
+});
 
 export default nextConfig;
