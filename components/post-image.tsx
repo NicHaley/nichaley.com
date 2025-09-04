c0f28433477925cd625ddb5ce3caf80b0d78b5c7
Nicholas Haley <hello@nichaley.com>
2025-09-04 10:29:15 -0400

    gradient

--- components/post-image.tsx
+++ components/post-image.tsx
@@ -4,7 +4,7 @@ type InlineImageProps = ImageProps;
 
 export default function PostImage({ src, alt }: InlineImageProps) {
   return (
-    <div className="flex flex-col gap-2 mb-4 p-8 aspect-video bg-stone-200 dark:bg-stone-700 rounded items-center justify-center">
+    <div className="flex flex-col gap-2 mb-4 p-4 md:p-8 aspect-video bg-linear-to-t from-stone-200 to-stone-100  dark:bg-stone-700 rounded items-center justify-center">
       <Image
         src={src}
         alt={alt ?? ""}
