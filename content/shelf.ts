6ddca86363eed4865aad4e4555e9ae3806848652
Nicholas Haley <hello@nichaley.com>
2025-10-14 23:48:44 -0400

    update lockfile and rerun

--- content/shelf.ts
+++ content/shelf.ts
@@ -1,134 +1,134 @@
 export const shelfSections = [
-	{
-		title: "Software",
-		bookmarks: [
-			{
-				title: "Cursor",
-				url: "https://www.cursor.com/",
-			},
-			{
-				title: "Drizzle",
-				url: "https://orm.drizzle.team/",
-			},
-			{
-				title: "Figma",
-				url: "https://www.figma.com/",
-			},
-			{
-				title: "Inngest",
-				url: "https://www.inngest.com/",
-			},
-			{
-				title: "Linear",
-				url: "https://www.linear.app/",
-			},
-			{
-				title: "Neon",
-				url: "https://neon.tech/",
-			},
-			{
-				title: "Next.js",
-				url: "https://nextjs.org/",
-			},
-			{
-				title: "Notion",
-				url: "https://www.notion.com/",
-			},
-			{
-				title: "React",
-				url: "https://react.dev/",
-			},
-			{
-				title: "shadcn",
-				url: "https://ui.shadcn.com/",
-			},
-			{
-				title: "Tailwind CSS",
-				url: "https://tailwindcss.com/",
-			},
-			{
-				title: "Turborepo",
-				url: "https://turborepo.com/",
-			},
-		],
-	},
-	{
-		title: "Hardware",
-		bookmarks: [
-			{
-				title: "Brompton",
-				url: "https://global.brompton.com",
-			},
-			{
-				title: "Fujifilm X100V",
-				url: "https://www.fujifilm-x.com/products/cameras/x100v/",
-			},
-			{
-				title: "M4 MacBook Pro",
-				url: "https://www.apple.com/macbook-pro-14-and-16/",
-			},
-			{
-				title: "Omnium Minimax",
-				url: "https://omniumcargo.com/mini-max/",
-			},
-			{
-				title: "Studio Display",
-				url: "https://www.apple.com/studio-display/",
-			},
-			{
-				title: "Technivorm Moccamaster",
-				url: "https://moccamaster.com/",
-			},
-		],
-	},
-	{
-		title: "Resources & Reads",
-		bookmarks: [
-			{
-				title: "Brutalist Web Design",
-				url: "https://brutalist-web.design/",
-			},
-			{
-				title: "Build: An Unorthodox Guide to Making Things Worth Making",
-				url: "https://www.goodreads.com/book/show/59696349-build",
-			},
-			{
-				title:
-					"Creative Selection: Inside Apple's Design Process During the Golden Age of Steve Jobs",
-				url: "https://www.goodreads.com/book/show/37638098-creative-selection",
-			},
-			{
-				title: "Gradient Generator",
-				url: "https://www.joshwcomeau.com/gradient-generator/",
-			},
-			{
-				title: "Hackers and Painters",
-				url: "https://www.paulgraham.com/hp.html",
-			},
-			{
-				title: "Motherfucking website",
-				url: "https://motherfuckingwebsite.com/",
-			},
-			{
-				title: "Refactoring UI",
-				url: "https://www.goodreads.com/book/show/43190966-refactoring-ui",
-			},
-			{
-				title: "Steve Jobs",
-				url: "https://www.goodreads.com/book/show/11084145-steve-jobs",
-			},
-			{
-				title: "The Creative Act: A Way of Being",
-				url: "https://www.goodreads.com/book/show/60965426-the-creative-act",
-			},
-			{
-				title: "The Grug Brained Developer",
-				url: "https://grugbrain.dev/",
-			},
-			{
-				title: "This is a web page",
-				url: "https://justinjackson.ca/words.html",
-			},
-		],
-	},
+  {
+    title: "Software",
+    bookmarks: [
+      {
+        title: "Cursor",
+        url: "https://www.cursor.com/",
+      },
+      {
+        title: "Drizzle",
+        url: "https://orm.drizzle.team/",
+      },
+      {
+        title: "Figma",
+        url: "https://www.figma.com/",
+      },
+      {
+        title: "Inngest",
+        url: "https://www.inngest.com/",
+      },
+      {
+        title: "Linear",
+        url: "https://www.linear.app/",
+      },
+      {
+        title: "Neon",
+        url: "https://neon.tech/",
+      },
+      {
+        title: "Next.js",
+        url: "https://nextjs.org/",
+      },
+      {
+        title: "Notion",
+        url: "https://www.notion.com/",
+      },
+      {
+        title: "React",
+        url: "https://react.dev/",
+      },
+      {
+        title: "shadcn",
+        url: "https://ui.shadcn.com/",
+      },
+      {
+        title: "Tailwind CSS",
+        url: "https://tailwindcss.com/",
+      },
+      {
+        title: "Turborepo",
+        url: "https://turborepo.com/",
+      },
+    ],
+  },
+  {
+    title: "Hardware",
+    bookmarks: [
+      {
+        title: "Brompton",
+        url: "https://global.brompton.com",
+      },
+      {
+        title: "Fujifilm X100V",
+        url: "https://www.fujifilm-x.com/products/cameras/x100v/",
+      },
+      {
+        title: "M4 MacBook Pro",
+        url: "https://www.apple.com/macbook-pro-14-and-16/",
+      },
+      {
+        title: "Omnium Minimax",
+        url: "https://omniumcargo.com/mini-max/",
+      },
+      {
+        title: "Studio Display",
+        url: "https://www.apple.com/studio-display/",
+      },
+      {
+        title: "Technivorm Moccamaster",
+        url: "https://moccamaster.com/",
+      },
+    ],
+  },
+  {
+    title: "Resources & Reads",
+    bookmarks: [
+      {
+        title: "Brutalist Web Design",
+        url: "https://brutalist-web.design/",
+      },
+      {
+        title: "Build: An Unorthodox Guide to Making Things Worth Making",
+        url: "https://www.goodreads.com/book/show/59696349-build",
+      },
+      {
+        title:
+          "Creative Selection: Inside Apple's Design Process During the Golden Age of Steve Jobs",
+        url: "https://www.goodreads.com/book/show/37638098-creative-selection",
+      },
+      {
+        title: "Gradient Generator",
+        url: "https://www.joshwcomeau.com/gradient-generator/",
+      },
+      {
+        title: "Hackers and Painters",
+        url: "https://www.paulgraham.com/hp.html",
+      },
+      {
+        title: "Motherfucking website",
+        url: "https://motherfuckingwebsite.com/",
+      },
+      {
+        title: "Refactoring UI",
+        url: "https://www.goodreads.com/book/show/43190966-refactoring-ui",
+      },
+      {
+        title: "Steve Jobs",
+        url: "https://www.goodreads.com/book/show/11084145-steve-jobs",
+      },
+      {
+        title: "The Creative Act: A Way of Being",
+        url: "https://www.goodreads.com/book/show/60965426-the-creative-act",
+      },
+      {
+        title: "The Grug Brained Developer",
+        url: "https://grugbrain.dev/",
+      },
+      {
+        title: "This is a web page",
+        url: "https://justinjackson.ca/words.html",
+      },
+    ],
+  },
 ];
--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,23 @@ import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	// Configure `pageExtensions` to include markdown and MDX files
-	pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-	experimental: {
-		mdxRs: true,
-		viewTransition: true,
-	},
-	images: {
-		remotePatterns: [
-			{
-				hostname: "www.google.com",
-			},
-		],
-	},
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  experimental: {
+    mdxRs: true,
+    viewTransition: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        hostname: "www.google.com",
+      },
+    ],
+  },
 };
 
 const withMDX = createMDX({
-	extension: /\.mdx?$/,
+  extension: /\.mdx?$/,
 });
 
 // Merge MDX config with Next.js config
