ee2425f406e8e5b0cac80986715b8f2bdb8ebb8e
Nicholas Haley <hello@nichaley.com>
2025-10-20 10:25:16 -0400

    wip - new blurb

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,26 @@ export default function Home() {
       // description="0 → 1 Product Engineer"
       // description="Developer and Designer"
     >
+      <p>
+        I'm a developer and designer based in Montreal [and currently visiting
+        X]. I'm currently on paternity leave. Previously, I was a founding
+        engineer at Ada and Upfront, and led frontend development at Local
+        Logic.
+      </p>
+      <p>
+        In my work, I explore how design and technology can shape better ways of
+        living, especially in our built environment. I also volunteer as a bike
+        mechanic and enjoy film and music.
+      </p>
       <p className="mb-10">
-        I&apos;m Nic — a product engineer based in Montreal 🥯 As an urbanist, I
-        love exploring the roles of technology and cycling in making our cities
-        greener and better places to live. Find me on{" "}
-        <a href="https://github.com/nichaley">GitHub</a> or{" "}
+        Find me on <a href="https://github.com/nichaley">GitHub</a> or{" "}
         <a href="https://www.linkedin.com/in/nicholas-haley-22757389/">
           LinkedIn
         </a>
         , or reach out to me at{" "}
         <a href="mailto:hello@nichaley.com">hello@nichaley.com</a>.
       </p>
-      <Stats />
+      {/* <Stats /> */}
     </Page>
   );
 }
