2b428c869840a95fdfd7d6572fecc8c555338743
Nicholas Haley <hello@nichaley.com>
2025-10-20 11:01:58 -0400

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Page from "@/components/page";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { getFirstDiaryEntry } from "@/lib/letterboxd";
 import { Stats } from "./stats";
 
 export const dynamic = "force-static";
 export const revalidate = 3600; // 1 hour
 
-export default function Home() {
+export default async function Home() {
+  const diaryEntry = await getFirstDiaryEntry();
+
   return (
     <Page
       title="Nic Haley"
@@ -12,9 +20,8 @@ export default function Home() {
       // description="Developer and Designer"
     >
       <p>
-        I'm a developer and designer based in Montreal [and currently visiting
-        X]. I'm currently on paternity leave. Previously, I was a founding
-        engineer at{" "}
+        I'm a developer and designer based in Montreal. I'm currently on
+        paternity leave. Previously, I was a founding engineer at{" "}
         <a href="https://ada.cx" target="_blank" rel="noopener">
           Ada
         </a>{" "}
@@ -22,7 +29,7 @@ export default function Home() {
         <a href="https://myupfront.com" target="_blank" rel="noopener">
           Upfront
         </a>
-        , and led frontend development at{" "}
+        , and led frontend dev at{" "}
         <a href="https://locallogic.co" target="_blank" rel="noopener">
           Local Logic
         </a>
@@ -31,7 +38,16 @@ export default function Home() {
       <p>
         In my work, I explore how design and technology can shape better ways of
         living, especially in our built environment. I also volunteer as a bike
-        mechanic and enjoy film and music.
+        mechanic and enjoy film and music. I last watched{" "}
+        <Tooltip>
+          <TooltipTrigger>
+            <span>film</span>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>film</p>
+          </TooltipContent>
+        </Tooltip>
+        .
       </p>
       <p className="mb-10">
         Find me on <a href="https://github.com/nichaley">GitHub</a> or{" "}
